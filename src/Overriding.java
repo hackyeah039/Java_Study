2b023f62eba01efceee4cf0d16a185da4ede0377
portfolio <jwjs3219@gmail.com>
2022-04-07 13:48:10 +0900

    first commit in overriding_vs_overloading

--- src/Overriding.java
+++ src/Overriding.java
@@ -1,5 +1,6 @@
 public class Overriding {
     public static void main(String[] args) {
         System.out.println("overriding_vs_overloading firstly commit for git");
+        System.out.println("change branch from master to overriding_vs_overloading");
     }
 }
